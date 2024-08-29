a76fef9b528da5adf91f1499da79870f837303b2
“Kuwado” <viethoan557723@gmail.com>
2024-08-29 23:58:02 +0700

    header

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+// import Logo from '~/assets/images/logo.svg';
+// export { Logo };
+
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    hoan: require('~/assets/images/hoan.jpg'),
+};
+
+export default images;
